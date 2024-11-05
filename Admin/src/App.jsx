b6b1d0719900581af23cd1ea037834c065b880cc
Shivam Kumar <123649432+shivamk23@users.noreply.github.com>
2024-11-05 17:29:01 +0530

    Update backend url in App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-   const url="https://foodie-backend-strn.onrender.com/"
+   const url="https://food-delivery-app-backend-7hco.onrender.com/"
 
   return (
     <div>
