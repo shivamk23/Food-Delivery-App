b62a1f0520f4f662af7c5c8c38053468598f46e5
Shivam Kumar <123649432+shivamk23@users.noreply.github.com>
2024-11-05 16:48:18 +0530

    remove extra back slashOrders.jsx

--- Admin/src/pages/Orders/Orders.jsx
+++ Admin/src/pages/Orders/Orders.jsx
@@ -10,7 +10,7 @@ const Orders = ({url}) => {
   const [orders,setOrders] = useState([]);
 
   const fetchAllOrders = async () => {
-    const response = await axios.get(url+"/api/order/list");
+    const response = await axios.get(url+"api/order/list");
     if(response.data.success){
       setOrders(response.data.data)
       console.log(response.data.data);
